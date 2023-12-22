b333f14c4aeb4b39bdf1bd814a8c45f9dd7e006e
Jack Shannon <j.b.shannon@lse.ac.uk>
2023-12-22 17:45:19 +0000

    restructured test folders

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,13 +3,12 @@ using AoCElfTools: samplepath, userpath
 using Test
 using Printf
 
-datadir = joinpath(@__DIR__, "data")
-
 function check_day(filepath)
     istr = filepath |> basename |> splitext |> first
     return parse(Int, istr), istr
 end
 
 @testset verbose=true "2022" begin
-    foreach(include, readdir("2022"; join=true))
+    datadir = joinpath(@__DIR__, "2022/data")
+    foreach(include, readdir("2022/tests"; join=true))
 end
