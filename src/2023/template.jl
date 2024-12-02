9443f391dddcc6e303216de1e4bce2ed8914abbb
Jack Shannon <j.b.shannon@lse.ac.uk>
2024-12-02 08:28:15 +0000

    template solution file

---
+++ src/2023/template.jl
@@ -0,0 +1,20 @@
+module Day0
+
+import ..parseday
+import ..solveday
+
+function parseday(::Val{0})
+    function f(io)
+        # content
+    end
+end
+
+function solveday(::Val{0})
+    function f(input)
+        ans₁ = nothing
+        ans₂ = nothing
+        return ans₁, ans₂
+    end
+end
+
+end # module Day0
